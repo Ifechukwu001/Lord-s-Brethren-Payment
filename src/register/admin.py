04f412f871eca682d32580d70b80a770700009dd
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-11 15:10:44 +0100

    chore: Update ParticipantAdmin in admin.py for better management of Participant model

--- src/register/admin.py
+++ src/register/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Participant
 
-# Register your models here.
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ["name", "user", "phone", "type", "has_paid"]
+    search_fields = ["name", "user.email", "phone"]
+    list_filter = ["type", "has_paid"]
--- src/register/models.py
+++ src/register/models.py
@@ -8,7 +8,7 @@ class Participant(models.Model):
         CNOD = "CNOD", "CNOD"
         PARTNER = "PTN", "Partner"
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     type = models.CharField(max_length=4, choices=Types.choices, default=Types.REGULAR)
