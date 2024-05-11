be91c9c42f3ddb714acaebb68c66325dd043b9e0
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-11 15:13:53 +0100

    chore: Update search fields in ParticipantAdmin for better filtering in admin interface

--- src/register/admin.py
+++ src/register/admin.py
@@ -5,5 +5,5 @@ from .models import Participant
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
     list_display = ["name", "user", "phone", "type", "has_paid"]
-    search_fields = ["name", "user.email", "phone"]
+    search_fields = ["name", "phone"]
     list_filter = ["type", "has_paid"]
