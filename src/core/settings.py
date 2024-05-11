08d2c63ad368b7d83310e140f55baeeedb1bb5da
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-11 14:53:55 +0100

    chore: Add accounts app to INSTALLED_APPS in settings.py

--- src/core/settings.py
+++ src/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Local apps
+    "accounts",
     "register",
     # Third-party apps
     "rest_framework",
@@ -128,3 +129,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
