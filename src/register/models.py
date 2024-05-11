e02bc8000feb7cb04e51428d7603071fcf5d6430
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-11 15:02:19 +0100

    chore: Add Participant model to register app

--- src/register/models.py
+++ src/register/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Participant(models.Model):
+    class Types(models.TextChoices):
+        REGULAR = "REG", "Regular"
+        CNOD = "CNOD", "CNOD"
+        PARTNER = "PTN", "Partner"
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    type = models.CharField(max_length=4, choices=Types.choices, default=Types.REGULAR)
+    has_paid = models.BooleanField(default=False)
