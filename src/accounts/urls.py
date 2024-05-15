aafc97d6af99fb8a17cb8a0914a1d27b698fc513
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-15 16:08:41 +0100

    feat: Add login and logout URL configuration to accounts app

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path, include
+from django.urls import path
+
+from .views import LoginView, LogoutView
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("auth/", include("dj_rest_auth.urls")),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
