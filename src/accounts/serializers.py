fbab489123b1b6ca70a2db493d53a79d4b0f763a
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-15 16:07:59 +0100

    feat: Add JWTSerializer and LoginSerializer to accounts app

---
+++ src/accounts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from dj_rest_auth.serializers import (
+    JWTSerializer as DJJWTSerializer,
+    LoginSerializer as DJLoginSerializer,
+)
+
+
+class JWTSerializer(DJJWTSerializer):
+    user = None
+
+
+class LoginSerializer(DJLoginSerializer):
+    username = None
