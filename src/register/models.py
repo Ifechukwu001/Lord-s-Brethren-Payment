3dcb5bbb0fdda87afc34f93e51873352ec3693fb
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-15 16:07:31 +0100

    feat: Remove 'type' field from Participant model and add 'category' field

--- src/register/models.py
+++ src/register/models.py
@@ -4,12 +4,13 @@ from django.db import models
 
 class Participant(models.Model):
     class Types(models.TextChoices):
-        REGULAR = "REG", "Regular"
-        CNOD = "CNOD", "CNOD"
-        PARTNER = "PTN", "Partner"
+        INVITEE = "Invitee"
+        MEMBER = "Member"
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
-    type = models.CharField(max_length=4, choices=Types.choices, default=Types.REGULAR)
+    category = models.CharField(
+        max_length=10, choices=Types.choices, default=Types.INVITEE
+    )
     has_paid = models.BooleanField(default=False)
