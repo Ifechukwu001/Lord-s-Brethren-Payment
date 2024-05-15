2c420dd7cd78ecc1d293d02979dfdef29131053a
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-15 16:09:06 +0100

    feat: Update accounts views with login and logout functionality

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from dj_rest_auth.views import LoginView as DJLoginView, LogoutView as DjLogoutView
+from rest_framework import serializers, exceptions
+from drf_spectacular.utils import extend_schema, inline_serializer
 
-# Create your views here.
+
+class LoginView(DJLoginView):
+    """
+    Check the credentials and return the REST Token
+    if the credentials are valid and authenticated.
+
+    Accept the following POST parameters: email, password and
+    return the JWT access and refresh tokens.
+    """
+
+    pass
+
+
+class LogoutView(DjLogoutView):
+    """
+    Logs out a user and deletes the JWT token.
+
+    Accepts the following POST parameters: refresh_token"""
+
+    @extend_schema(exclude=True)
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        request=inline_serializer(
+            "Logout",
+            {"refresh": serializers.CharField(required=True)},
+        )
+    )
+    def post(self, request, *args, **kwargs):
+        refresh = request.data.get("refresh")
+        if not refresh:
+            raise exceptions.ValidationError("Refresh token is required")
+        return super().post(request, *args, **kwargs)
