f9fef26363bd718a26e0510d2ec8cac8fb4d083e
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-18 17:13:37 +0100

    feat: Add extend_schema decorator to TransactionCreateView

--- src/payments/views.py
+++ src/payments/views.py
@@ -1,5 +1,6 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions, status, serializers
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema, inline_serializer
 
 from .models import Transaction
 from .serializers import TransactionSerializer
@@ -10,6 +11,24 @@ class TransactionCreateView(generics.GenericAPIView):
     queryset = Transaction.objects.all()
     permission_classes = [permissions.AllowAny]
 
+    @extend_schema(
+        responses={
+            201: inline_serializer(
+                name="TransactionLink",
+                fields={
+                    "status": serializers.CharField(),
+                    "link": serializers.URLField(),
+                },
+            ),
+            424: inline_serializer(
+                name="TransactionFailed",
+                fields={
+                    "status": serializers.CharField(),
+                    "message": serializers.CharField(),
+                },
+            ),
+        }
+    )
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
