a06a01ee9c446ff8cb27d1d5c0d0e4a250d43e9f
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-22 14:00:45 +0100

    feat: Add TransactionWebHook view for payment webhook handling

--- src/payments/urls.py
+++ src/payments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import TransactionCreateView
+from .views import TransactionCreateView, TransactionWebHook
 
 urlpatterns = [
     path("payment/", TransactionCreateView.as_view(), name="start_transaction"),
+    path("payment-hook/", TransactionWebHook.as_view(), name="webhook"),
 ]
