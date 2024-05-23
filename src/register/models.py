1699f28f895d2e933cae632717eb26746b5e6000
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-23 16:01:38 +0100

    feat: Refactor Participant model

--- src/register/models.py
+++ src/register/models.py
@@ -1,16 +1,48 @@
 from django.conf import settings
 from django.db import models
 
+from payments.models import Transaction
+
 
 class Participant(models.Model):
     class Types(models.TextChoices):
         INVITEE = "Invitee"
         MEMBER = "Member"
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
+    class Gender(models.TextChoices):
+        MALE = "M"
+        FEMALE = "F"
+
+    class Attendance(models.TextChoices):
+        CAMPER = "Camper"
+        DAILY = "Daily"
+        STREAMER = "Streamer"
+
+    class Reach(models.TextChoices):
+        FACEBOOK = "Facebook"
+        WHATSAPP = "WhatsApp"
+        INSTAGRAM = "Instagram"
+        CHURCH = "Church"
+        WEBSITE = "Website"
+        OUTREACH = "Outreach"
+        FRIEND = "Friend"
+
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
     phone = models.CharField(max_length=15)
+    gender = models.CharField(max_length=2, choices=Gender.choices, default=Gender.MALE)
+    birthdate = models.DateField()
+    address = models.CharField(max_length=255)
     category = models.CharField(
         max_length=10, choices=Types.choices, default=Types.INVITEE
     )
-    has_paid = models.BooleanField(default=False)
+    church_name = models.CharField(max_length=100)
+    attendance_mode = models.CharField(
+        max_length=10, choices=Attendance.choices, default=Attendance.CAMPER
+    )
+    was_participant = models.BooleanField(default=False)
+    is_aware_of_convention = models.BooleanField(default=False)
+    health_issue = models.TextField(null=True, blank=True)
+    reach = models.CharField(max_length=10, choices=Reach.choices, default=Reach.CHURCH)
+    transaction = models.OneToOneField(Transaction, on_delete=models.CASCADE, null=True)
