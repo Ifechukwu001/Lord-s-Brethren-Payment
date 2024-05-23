bb5497af28f9ed1588d77fa3913f1c48d0ffcdac
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-23 16:01:18 +0100

    feat: Refactor ParticipantAdmin in register/admin.py

--- src/register/admin.py
+++ src/register/admin.py
@@ -4,6 +4,4 @@ from .models import Participant
 
 @admin.register(Participant)
 class ParticipantAdmin(admin.ModelAdmin):
-    list_display = ["name", "user", "phone", "category", "has_paid"]
-    search_fields = ["name", "phone"]
-    list_filter = ["category", "has_paid"]
+    pass
