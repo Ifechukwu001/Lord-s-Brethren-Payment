2523b335f40a42985a6fe851294626186d9771e2
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-25 15:20:43 +0100

    feat: Update TransactionCreateView to generate payment link

--- src/payments/views.py
+++ src/payments/views.py
@@ -41,21 +41,19 @@ class TransactionCreateView(generics.GenericAPIView):
         transaction = serializer.create()
 
         title = "Partnership payment"
-        if transaction.for_ticket:
-            title = "Payment for ticket"
-
         link = transaction.generate_payment_link(title=title)
         if not link:
             response_data = {"status": "failed", "message": "An error occurred"}
             return Response(response_data, status=status.HTTP_424_FAILED_DEPENDENCY)
-        response_data = {"status": "success", "link": link}
 
+        response_data = {"status": "success", "link": link}
         return Response(response_data, status=status.HTTP_201_CREATED)
 
 
 class TransactionWebHook(generics.GenericAPIView):
     permission_classes = [permissions.AllowAny]
 
+    @extend_schema(exclude=True)
     def post(self, request, *args, **kwargs):
         hook_hash = config("FLUTTERWAVE_WEBHOOK_HASH")
         signature = request.headers.get("verif-hash")
