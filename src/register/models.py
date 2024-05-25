24de1296ae49915ad56f1ee0d8fbae486f1ccbe4
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-25 14:22:42 +0100

    Refactor Participant model and add generate_payment_link method

--- src/register/models.py
+++ src/register/models.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.db import models
 
 from payments.models import Transaction
@@ -46,3 +45,21 @@ class Participant(models.Model):
     health_issue = models.TextField(null=True, blank=True)
     reach = models.CharField(max_length=10, choices=Reach.choices, default=Reach.CHURCH)
     transaction = models.OneToOneField(Transaction, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
+
+    def generate_payment_link(self):
+        title = "Payment for Convention"
+        if not self.transaction:
+            if self.category == self.Types.MEMBER:
+                amount = 5000
+            else:
+                amount = 2000
+
+            self.transaction = Transaction.objects.create(
+                email=self.email, amount=amount, currency="NGN", for_ticket=True
+            )
+            self.save()
+
+        return self.transaction.generate_payment_link(title)
