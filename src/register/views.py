671cf40054da022f07fdc0d2d81e1f6e20a1529e
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-27 13:43:39 +0100

    feat: Update RegisterView to inherit from DJRegisterView

--- src/register/views.py
+++ src/register/views.py
@@ -1,40 +1,28 @@
 from rest_framework import generics, status, serializers
 from rest_framework.response import Response
 from drf_spectacular.utils import extend_schema, inline_serializer
+from dj_rest_auth.registration.views import RegisterView as DJRegisterView
 
 from .serializers import RegisterSerializer
 
 
-class RegisterView(generics.GenericAPIView):
-    serializer_class = RegisterSerializer
-
+class RegisterView(DJRegisterView):
     @extend_schema(
         responses={
             201: inline_serializer(
-                name="RegisterTransactionLink",
+                name="RegisterResponse",
                 fields={
-                    "status": serializers.CharField(),
-                    "link": serializers.URLField(),
-                },
-            ),
-            424: inline_serializer(
-                name="RegisterTransactionFailed",
-                fields={
-                    "status": serializers.CharField(),
-                    "message": serializers.CharField(),
+                    "access": serializers.CharField(),
+                    "refresh": serializers.CharField(),
                 },
             ),
         }
     )
     def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        return super().post(request)
 
-        participant = serializer.save()
-        link = participant.generate_payment_link()
-        if not link:
-            response_data = {"status": "failed", "message": "An error occurred"}
-            return Response(response_data, status=status.HTTP_424_FAILED_DEPENDENCY)
-
-        response_data = {"status": "success", "link": link}
-        return Response(response_data, status=status.HTTP_201_CREATED)
+    def get_response_data(self, user):
+        data = super().get_response_data(user)
+        if "user" in data:
+            data.pop("user")
+        return data
