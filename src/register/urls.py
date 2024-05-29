16c5c6413a8f6fab2efc48ed2ce16be0d174ed68
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-29 10:20:31 +0100

    feat: Add ParticipantAPIView for retrieving and updating participant information

--- src/register/urls.py
+++ src/register/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import RegisterView, GenerateTicketPaymentAPIView
+from .views import RegisterView, ParticipantAPIView, GenerateTicketPaymentAPIView
 
 app_name = "register"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path("participant/", ParticipantAPIView.as_view(), name="participant"),
     path(
         "ticket-payment/",
         GenerateTicketPaymentAPIView.as_view(),
