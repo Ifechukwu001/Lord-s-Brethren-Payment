4ead0a62000888eacff4cff7440c71bbe4a70656
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-05-29 10:09:01 +0100

    feat: Add endpoint for generating ticket payment links

--- src/register/urls.py
+++ src/register/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import RegisterView
+from .views import RegisterView, GenerateTicketPaymentAPIView
 
 app_name = "register"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path(
+        "ticket-payment/",
+        GenerateTicketPaymentAPIView.as_view(),
+        name="generate-ticket-payment",
+    ),
 ]
