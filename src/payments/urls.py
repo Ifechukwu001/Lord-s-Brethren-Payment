df37083a6a3b488a1b948c7777827830adf04a4e
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-06-24 14:52:53 +0100

    feat: Add endpoint for verifying transaction

--- src/payments/urls.py
+++ src/payments/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import TransactionCreateView, TransactionWebHook
+from .views import TransactionCreateView, TransactionWebHook, TransactionVerifyAPIView
 
 urlpatterns = [
     path("payment/", TransactionCreateView.as_view(), name="start_transaction"),
+    path(
+        "payment/verify/", TransactionVerifyAPIView.as_view(), name="verify_transaction"
+    ),
     path("payment-hook/", TransactionWebHook.as_view(), name="webhook"),
 ]
