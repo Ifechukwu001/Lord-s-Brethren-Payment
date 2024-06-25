587d3fc0c356c927a6db291b9cdb723d6bab9352
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-06-25 17:01:40 +0100

    refactor: Update participant URL pattern to include reference parameter
    The participant URL pattern in the register app has been updated to include a reference parameter. This change allows for more specific routing and handling of participant-related requests. The reference parameter is a string that uniquely identifies a participant. This commit addresses the need to enhance the participant functionality by incorporating a reference parameter in the URL pattern.

--- src/register/urls.py
+++ src/register/urls.py
@@ -6,10 +6,12 @@ app_name = "register"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("participant/", ParticipantAPIView.as_view(), name="participant"),
     path(
-        "ticket-payment/",
-        GenerateTicketPaymentAPIView.as_view(),
-        name="generate-ticket-payment",
+        "participant/<str:reference>/", ParticipantAPIView.as_view(), name="participant"
     ),
+    # path(
+    #     "ticket-payment/",
+    #     GenerateTicketPaymentAPIView.as_view(),
+    #     name="generate-ticket-payment",
+    # ),
 ]
