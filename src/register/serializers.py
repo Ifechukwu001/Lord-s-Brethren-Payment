40e068c451b6308e52668dd0462bb1b3202e9363
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-07-04 07:32:36 +0100

    feat: Add Partner model and migration

--- src/register/serializers.py
+++ src/register/serializers.py
@@ -1,10 +1,11 @@
+from decimal import Decimal
 from rest_framework import serializers
+import moneyed
 
-from .models import Participant
+from .models import Participant, Partner
 
 
-class RegisterSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(write_only=True)
+class ParticipantRegisterSerializer(serializers.ModelSerializer):
     callback_url = serializers.URLField(required=False)
 
     class Meta:
@@ -33,8 +34,42 @@ class RegisterSerializer(serializers.ModelSerializer):
         return super().save()
 
 
+class PartnerRegisterSerializer(serializers.ModelSerializer):
+    callback_url = serializers.URLField(required=False)
+    currency = serializers.ChoiceField(
+        choices=[
+            (currency.code, currency.name) for currency in moneyed.list_all_currencies()
+        ],
+        default="NGN",
+    )
+    amount = serializers.DecimalField(
+        min_value=Decimal(0.00), max_digits=10, decimal_places=2
+    )
+
+    class Meta:
+        model = Partner
+        fields = [
+            "name",
+            "email",
+            "phone",
+            "country",
+            "state",
+            "currency",
+            "amount",
+            "callback_url",
+        ]
+
+    def save(self):
+        if "callback_url" in self.validated_data:
+            self.validated_data.pop("callback_url")
+        if "currency" in self.validated_data:
+            self.validated_data.pop("currency")
+        if "amount" in self.validated_data:
+            self.validated_data.pop("amount")
+        return super().save()
+
+
 class ParticipantSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(read_only=True)
     category = serializers.CharField(read_only=True)
 
     class Meta:
@@ -54,6 +89,19 @@ class ParticipantSerializer(serializers.ModelSerializer):
             "is_aware_of_convention",
             "health_issue",
             "reach",
+            "has_paid",
+        ]
+
+
+class PartnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Partner
+        fields = [
+            "name",
+            "email",
+            "phone",
+            "country",
+            "state",
             "has_paid",
         ]
 
