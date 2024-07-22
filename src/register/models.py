b67aef55e218f3b5f6fa2ddf19129ffa61de3fb9
Ifechukwu T. Ogidi <ogidiifechukwu6@gmail.com>
2024-07-22 19:36:17 +0100

    Make email and phone unique

--- src/register/models.py
+++ src/register/models.py
@@ -32,8 +32,8 @@ class Participant(models.Model):
 
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, unique=True)
     gender = models.CharField(max_length=2, choices=Gender.choices, default=Gender.MALE)
     birthdate = models.DateField()
     address = models.CharField(max_length=255)
